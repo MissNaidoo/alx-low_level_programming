432a1f326700912dec3f9a65791d215978062da6
MissNaidoo <miss.naidoo@rocketmail.com>
2023-02-17 05:42:05 -0800

    Task 6 Script

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void);
+{
+	char = a;
+	int = b;
+	long int = c;
+	long long int = d;
+	float = f;
+
+	printf("Size of a char: %lu Byte(s)\n", (unsigned long)size of(a));
+	printf("Size of a int: %lu Byte(s)\n", (unsigned long)size of(b));
+	printf("Size of a long int: %lu Byte(s)\n", (unsigned long)size of(c));
+	printf("Size of a long long int: %lu Byte(s)\n", (unsigned long)size of(d));
+	printf("Size of a float: %lu Byte(s)\n", (unsigned long)size of(f));
+	return (0);
+
+}
+
