e33c293a3925ffac73b9d1228e0adc895310b123
MissNaidoo <miss.naidoo@rocketmail.com>
2023-02-23 12:34:54 -0800

    Task 3 redoo

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 # include "main.h"
 
 /**
- * print_numbers - check if number 0 - 9 prints
+ * print_numbers - print 0 - 9
  * Return: void
  */
 
@@ -9,7 +9,7 @@ void print_numbers(void)
 {
 	char i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		_putchar(c);
 	}
