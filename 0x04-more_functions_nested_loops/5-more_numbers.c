0ffc243b37185fa4f442b4ee2c8da759455f8933
MissNaidoo <miss.naidoo@rocketmail.com>
2023-02-23 11:21:56 -0800

    Task 5 REDO

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,26 +2,20 @@
 
 /**
  * more_numbers - Prints 10 times the numbers since 0 up to 14
- * @x: int to check
- * @y: int to check
- *
- * Return: 0 success
  */
 
 void more_numbers(void);
 {
-	int i;
-	int j;
+	int i, j;
 
-	while (i <= 9)
+	for (i = 1; i <= 10; i++)
 	{
-		j = 0;
-		while (j <= 14)
-			{
-			_putchar("\%d", j);
-			j++;
-			}
-		_putchar("\n");
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
 	}
-	return (0);
 }
