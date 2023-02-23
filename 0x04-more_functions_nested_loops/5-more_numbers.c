cfbfd476723aa871c9c3453fb8dbda31a47f3f6e
MissNaidoo <miss.naidoo@rocketmail.com>
2023-02-23 13:37:13 -0800

    Task 5e

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,11 @@ void more_numbers(void);
 {
 	int i, j;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 1; j <= 14; j++)
 		{
-			if (j >= 10)
+			if (j >= 9)
 				_putchar('1');
 			_putchar(j % 10 + '0');
 		}
