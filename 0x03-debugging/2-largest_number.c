175541f11e6c572ad42975179c0743986901f3f7
MissNaidoo <miss.naidoo@rocketmail.com>
2023-02-23 13:31:54 -0800

    Redo Task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,7 +12,7 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b && a > c)
 	{
 	largest = a;
 	}
@@ -27,7 +27,6 @@ int largest_number(int a, int b, int c)
 	else
 	{
 	largest = c;
-    }
-
+	}
 return (largest);
 }
