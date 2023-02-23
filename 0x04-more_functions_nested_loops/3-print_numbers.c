b281075ae0248c522937ed0c9fefec04dc75f1f2
MissNaidoo <miss.naidoo@rocketmail.com>
2023-02-23 06:48:24 -0800

    Task 3 Script

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+# include "main.h"
+
+/**
+ * 3-print_numbers - check if number 0 - 9 prints
+ * @c: char to be checked
+ *
+ * Return: 1
+ */
+
+void print_numbers(void)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return ('\n');
+}
