70ac1d185e00b2474c34a3e607ed3cc8e37498a8
MissNaidoo <miss.naidoo@rocketmail.com>
2023-02-24 09:50:46 -0800

    REdo edit Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,17 @@
 
 void more_numbers(void);
 {
-	int i, j;
+	int i = 0, j;
 
-	for (i = 1; i <= 10; i++)
+	while (i < 10)
 	{
-		for (j = 1; j <= 14; j++)
+		j = 0;
+		while (j <= 14)
 		{
-			if (j >= 10)
-				_putchar('1');
-			_putchar(j % 10 + '0');
+			_putchar(j);
+			j++;
 		}
 		_putchar('\n');
+		i++;
 	}
 }
