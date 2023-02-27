bc59fd3e7b46537ca73e9493ef457a55e7b42aab
MissNaidoo <miss.naidoo@rocketmail.com>
2023-02-27 01:54:42 -0800

    Task 0 Script

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a point to an int and updates
+ * the value it points to is 98
+ * @n: an in input pointer
+ * Return: int n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
