17c1fe65ecd23d8b10cf7dbc4eda77fd7d95dc07
MissNaidoo <miss.naidoo@rocketmail.com>
2023-03-01 05:06:00 -0800

    Task 1 Script

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - Concatenates two strings
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * Return: char dest and src
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+while (dest[i] != '\0')
+{
+i++;
+}
+
+while (j < n && src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
+}
