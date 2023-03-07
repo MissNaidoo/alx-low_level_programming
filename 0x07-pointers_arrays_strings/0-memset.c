c9868385983487a2c98f60e3448430160c21be9b
MissNaidoo <miss.naidoo@rocketmail.com>
2023-03-07 07:07:11 -0800

    Task 0 Script

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memset - Entry point
+ * @s: starting add of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ * Return: always 0 (success)
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	s[i] = b;
+	return (s);
+}
