fb6b12de80f9c8460cdd3d4a9a9fd114929c5dc8
MissNaidoo <miss.naidoo@rocketmail.com>
2023-03-14 11:53:58 -0700

    Task 0 Script

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", *argv);
+	return (0);
+}
